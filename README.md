f774f71fbc75946d056d44a64d54ab9b3a71df63
Robert van Reems <robertreems@gmail.com>
2022-04-17 16:50:59 +0200

    formatting, notification now based on average and README

--- README.md
+++ README.md
@@ -1,3 +1,37 @@
-# rvrlogging
+# RVR power surplus logger
 
-Logging to Azure.
+Average dutch households consume about 30% of the power it produces with solar panels. My goal is to raise that percentage so I'm consuming the power that's been produced by my own house.
+I'd like to use it for my home's climate controll and charging the car for example.
+
+This script logs the power surplus, the power I deliver to the enery grid. The logging goes to my Azure log analytics workspace and it sends browsernotifications. These notifications are to inform me when to turn house hold appliances on or off to consume the produced Power myself.
+For example when I'll have to start or stop the airco / car charging.
+
+My ultimate goal is to automate all this. But hey you should set some goals.
+
+The power surpluss is measured using a [Home Wizard P1 wifi dongle](https://www.homewizard.nl/shop/homewizard-wi-fi-p1-meter). 
+
+## Prerequistes
+[The rvr-base module](https://github.com/robertreems/rvrbase)
+
+This script is developed for Linux and tested on:
+- Ubuntu 20.04 using Python 3.8.10.
+- An Azure log analytics workspace.
+- Raspberry Pi OS using Python 3.9.2.
+- a smart energy meter in your home that's compatible with:
+- a Home wizard P1 meter.
+
+## Using this script
+It reads a configuration file. You'll have to create that first in /etc/rvr/config.ini. Make sure it has the following contents:
+```
+[AZ_LOG_ANALYTICS_WORKSPACE]
+workspace_id = AZ WORKSPACE
+primary_key = PRIMAIRY KEY
+
+[HOME_WIZARD_P1]
+hwip = IP OF HOME WIZARD DONGLE
+```
+
+Then you're able to run the main.py script file.
+
+## Disclaimer
+This script has been written for my personal use. Feel free to use it but at your own discression. There is no support or whatsoever on my part. 
--- rvrlogging/main.py
+++ rvrlogging/main.py
@@ -14,12 +14,16 @@ ip = conf.q1('hwip')
 thelogger = rvrlogger.Rvrlogger()
 is_no_power_notification_send = False
 
-# The initial plan was to use the '/api/v1/data' api to retrieve my solar power surplus from one of the api properties written in 
+# The initial plan was to use the '/api/v1/data' api to retrieve my solar power surplus from one of the api properties written in
 # https://homewizard-energy-api.readthedocs.io/endpoints.html.
-# Unfortunately my "smart" energymeter is very old and provides telegram messages with very limited information. So the '/api/v1/data' 
-# doesnt' contain any usable information. The mobile app doesn't show the current power production as well. So I ended up interpreting
-# the telegram messages and calculate the surplus based on the provided telegrams. The telegrams are documented here: 
-# https://www.netbeheernederland.nl/_upload/Files/Slimme_meter_15_a727fce1f1.pdf. 
+# Unfortunately my "smart" energymeter provides telegram messages with very limited information. So the '/api/v1/data' doesnt' contain
+# any usable information. The mobile app doesn't show the current power production as well. So I ended up interpreting the telegram
+# messages and calculate the surplus based on the provided telegrams. The telegrams are documented here:
+# https://www.netbeheernederland.nl/_upload/Files/Slimme_meter_15_a727fce1f1.pdf.
+
+
+def Average(lst):
+    return sum(lst) / len(lst)
 
 
 active_power_history = list()
@@ -43,8 +47,10 @@ while True:
     # Log the current power
     active_power_tariff1 = int(((end_tariff1 - start_tariff1) * 60) * 1000)
     active_power_tariff2 = int(((end_tariff2 - start_tariff2) * 60) * 1000)
-    thelogger.post_metric('power_usage', 'active_power_surplus_t1', active_power_tariff1)
-    thelogger.post_metric('power_usage', 'active_power_surplus_t2', active_power_tariff2)
+    thelogger.post_metric(
+        'power_usage', 'active_power_surplus_t1', active_power_tariff1)
+    thelogger.post_metric(
+        'power_usage', 'active_power_surplus_t2', active_power_tariff2)
 
     # Send a notification message if there is no energy surplus for a prolonged time.
     active_power_history.insert(0, active_power_tariff1)
@@ -53,11 +59,13 @@ while True:
     # remove the measurements after x measures.
     del active_power_history[20:]
 
-    if sum(active_power_history) == 0 and is_no_power_notification_send == False:
-        thelogger.log_application_event(type='info', message='No power surplus for a prolonged time.', notify_message=True)
+    if sum(active_power_history) == 0 and is_no_power_notification_send is False:
+        thelogger.log_application_event(
+            type='info', message='No power surplus for a prolonged time.', notify_message=True)
         is_no_power_notification_send = True
-    
+
     # Reset is_no_power_notification_send to false in order to send message if there is no longer any surplus.
-    elif sum(active_power_history) > 1000 and is_no_power_notification_send == True:
-        thelogger.log_application_event(type='info', message=f'You have got some juice. {sum(active_power_history)} Watts surplus in the past 10 minutes. Use it!', notify_message=True)
+    elif Average(active_power_history) > 500 and is_no_power_notification_send is True:
+        thelogger.log_application_event(
+            type='info', message=f'You have got some juice. {sum(active_power_history)} Watts surplus in the past 10 minutes. Use it!', notify_message=True)
         is_no_power_notification_send = False
