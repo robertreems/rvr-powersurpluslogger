f241e161863d00fedb9a1d1ac9f0922e82c58172
Robert van Reems <robertreems@gmail.com>
2022-04-24 20:38:51 +0200

    pylama

--- rvrlogging/constants.py
+++ rvrlogging/constants.py
@@ -1,5 +1,6 @@
-﻿CONFIG_FILE = '/etc/rvr/config.ini'
+CONFIG_FILE = '/etc/rvr/config.ini'
 
 # Errors
-ERR_UNEXPECTED_HTTP_RESPONSE = 'An unexpected HTTP response was received from the P1 dongle: {response}'
+ERR_UNEXPECTED_HTTP_RESPONSE = 'An unexpected HTTP response was received from the P1 dongle: \
+{response}'
 ERR_UNKNOWN_ERROR = 'An unexpected Error happened with message: {message}'
