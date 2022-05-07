93225fa24a36dd046b3b0ab00fc10da4d9a28cb2
Robert van Reems <robertreems@gmail.com>
2022-05-07 07:51:47 +0200

    one function get_power_meter

--- rvrstatisticslogger/main.py
+++ rvrstatisticslogger/main.py
@@ -1,5 +1,6 @@
 import logging
 from rvrbase import Rvrbase
+import datetime
 
 import constants
 
@@ -7,40 +8,22 @@ logging.getLogger().setLevel(logging.INFO)
 
 base = Rvrbase(constants.CONFIG_FILE)
 
-def get_meters_start_day(row):
-    # consumption_tariff1 = base.azlog_analyticsq("power_usage_CL | where TimeGenerated > startofday(now()) and  metric_name_s == 'consumption_tariff1' | sort by TimeGenerated asc | limit 1")
-    # consumption_tariff2 = base.azlog_analyticsq("power_usage_CL | where TimeGenerated > startofday(now()) and  metric_name_s == 'consumption_tariff2' | sort by TimeGenerated asc | limit 1")
-    # delivery_tariff1 = base.azlog_analyticsq("power_usage_CL | where TimeGenerated > startofday(now()) and  metric_name_s == 'delivery_tariff1' | sort by TimeGenerated asc | limit 1")
-    # delivery_tariff2 = base.azlog_analyticsq("power_usage_CL | where TimeGenerated > startofday(now()) and  metric_name_s == 'delivery_tariff2' | sort by TimeGenerated asc | limit 1")
 
-    result = base.azlog_analyticsq(f"power_usage_CL | where TimeGenerated > startofday(now()) and  metric_name_s == '{row}' | sort by TimeGenerated asc | limit 1")
-    # consumption_tariff1['tables'][0]['rows'][0][11]
+def get_power_meter(row, date, sort_order):
+    # todo validate sort_order.
+
+    day = date.strftime("%Y-%m-%d")
+
+    query = f"power_usage_CL | where TimeGenerated > startofday(datetime({day})) \
+        and TimeGenerated < endofday(datetime({day}))\
+        and metric_name_s == '{row}'\
+        | sort by TimeGenerated {sort_order}\
+        | limit 1"
+
+    result = base.azlog_analyticsq(query=query)
     return result['tables'][0]['rows'][0][11]
 
 
-# bovenstaande functie uitbreiden met deze werkende query:
-# power_usage_CL | where TimeGenerated > startofday(datetime(2022-05-05)) 
-# and TimeGenerated < startofday(datetime(2022-05-06))
-# and metric_name_s == 'consumption_tariff2' | sort by TimeGenerated desc | limit 1
-# Dit door tijd en sort aan te passen kun je heel gemakkelijk de dag en begin en einde van de dag selecteren.
 
-
-
-def get_meters_end_day(row):
-    try:
-        result = base.azlog_analyticsq(f"power_usage_CL | where TimeGenerated > startofday(now()) and  metric_name_s == '{row}' | sort by TimeGenerated desc | limit 1")
-        # consumption_tariff1['tables'][0]['rows'][0][11]
-        return result['tables'][0]['rows'][0][11]
-    except IndexError as error:
-        base.send_az_app_event(type='warning', message=f'No metric found for end of day meters row: {row}')
-
-print(get_meters_start_day('consumption_tariff1'))
-print(get_meters_start_day('consumption_tariff2'))
-print(get_meters_start_day('delivery_tariff1'))
-print(get_meters_start_day('delivery_tariff2'))
-
-
-print(get_meters_end_day('consumption_tariff1'))
-print(get_meters_end_day('consumption_tariff2'))
-print(get_meters_end_day('delivery_tariff1'))
-print(get_meters_end_day('delivery_tariff2'))
+print(get_power_meter('delivery_tariff2', datetime.datetime(2022, 5, 3), 'asc'))
+print(get_power_meter('delivery_tariff2', datetime.datetime(2022, 5, 3), 'desc'))
