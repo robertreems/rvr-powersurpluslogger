459bc6f24f7b45d84bbd144f2e1f528bbc63d1be
Robert van Reems <robertreems@gmail.com>
2022-05-07 07:09:55 +0200

    werkende voorbeelden

--- rvrstatisticslogger/main.py
+++ rvrstatisticslogger/main.py
@@ -18,8 +18,29 @@ def get_meters_start_day(row):
     return result['tables'][0]['rows'][0][11]
 
 
+# bovenstaande functie uitbreiden met deze werkende query:
+# power_usage_CL | where TimeGenerated > startofday(datetime(2022-05-05)) 
+# and TimeGenerated < startofday(datetime(2022-05-06))
+# and metric_name_s == 'consumption_tariff2' | sort by TimeGenerated desc | limit 1
+# Dit door tijd en sort aan te passen kun je heel gemakkelijk de dag en begin en einde van de dag selecteren.
+
+
+
+def get_meters_end_day(row):
+    try:
+        result = base.azlog_analyticsq(f"power_usage_CL | where TimeGenerated > startofday(now()) and  metric_name_s == '{row}' | sort by TimeGenerated desc | limit 1")
+        # consumption_tariff1['tables'][0]['rows'][0][11]
+        return result['tables'][0]['rows'][0][11]
+    except IndexError as error:
+        base.send_az_app_event(type='warning', message=f'No metric found for end of day meters row: {row}')
+
 print(get_meters_start_day('consumption_tariff1'))
 print(get_meters_start_day('consumption_tariff2'))
 print(get_meters_start_day('delivery_tariff1'))
 print(get_meters_start_day('delivery_tariff2'))
-    
+
+
+print(get_meters_end_day('consumption_tariff1'))
+print(get_meters_end_day('consumption_tariff2'))
+print(get_meters_end_day('delivery_tariff1'))
+print(get_meters_end_day('delivery_tariff2'))
