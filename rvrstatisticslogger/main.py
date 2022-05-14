b2581610840a798bc9d3cf86535d47999af9f393
Robert van Reems <robertreems@gmail.com>
2022-05-14 10:04:18 +0200

    refactor functions to be reused

--- rvrstatisticslogger/main.py
+++ rvrstatisticslogger/main.py
@@ -23,42 +23,42 @@ def get_power_meter(row, date, sort_order):
     return result['tables'][0]['rows'][0][11]
 
 
-def calculate_meter_daily_total(meter, date):
-    start_daily_measure = get_power_meter(meter, date, 'asc')
-    end_daily_measure = get_power_meter(meter, date, 'desc')
+def calculate_meter_total(meter, start_date, end_date):
+    start_daily_measure = get_power_meter(meter, start_date, 'asc')
+    end_daily_measure = get_power_meter(meter, end_date, 'desc')
 
     return round((end_daily_measure - start_daily_measure), 3)
 
 
 # Get all meter daily totals.
-def day_statistics(day_to_monitor):
-    dt_consumption_tariff1 = calculate_meter_daily_total(
-        'consumption_tariff1', day_to_monitor)
-    dt_consumption_tariff2 = calculate_meter_daily_total(
-        'consumption_tariff2', day_to_monitor)
-    dt_delivery_tariff1 = calculate_meter_daily_total(
-        'delivery_tariff1', day_to_monitor)
-    dt_delivery_tariff2 = calculate_meter_daily_total(
-        'delivery_tariff2', day_to_monitor)
+def statistics(start_date, end_date):
+    consumption_tariff1 = calculate_meter_total(
+        'consumption_tariff1', start_date, end_date)
+    consumption_tariff2 = calculate_meter_total(
+        'consumption_tariff2', start_date, end_date)
+    delivery_tariff1 = calculate_meter_total(
+        'delivery_tariff1', start_date, end_date)
+    delivery_tariff2 = calculate_meter_total(
+        'delivery_tariff2', start_date, end_date)
 
     # The daily power consumption / production combined.
-    day_total_consumption = dt_consumption_tariff1 + dt_consumption_tariff2
-    day_total_delivery = dt_delivery_tariff1 + dt_delivery_tariff2
+    day_total_consumption = consumption_tariff1 + consumption_tariff2
+    day_total_delivery = delivery_tariff1 + delivery_tariff2
     daily_total = round((day_total_delivery - day_total_consumption), 3)
 
     # I want to log these values to Azure daily.
     print(f'Daily total: {daily_total}')
     print(
-        f'Daily total tariff 1: {dt_delivery_tariff1 - dt_consumption_tariff1}')
+        f'Daily total tariff 1: {delivery_tariff1 - consumption_tariff1}')
     print(
-        f'Daily total tariff 2: {dt_delivery_tariff2 - dt_consumption_tariff2}')
-    print(dt_consumption_tariff1)
-    print(dt_consumption_tariff2)
-    print(dt_delivery_tariff1)
-    print(dt_delivery_tariff2)
+        f'Daily total tariff 2: {delivery_tariff2 - consumption_tariff2}')
+    print(consumption_tariff1)
+    print(consumption_tariff2)
+    print(delivery_tariff1)
+    print(delivery_tariff2)
 
     # base.send_az_metric('power_usage', 'consumption_tariff1', start_c_t1)
-    # print(calculate_meter_daily_total('delivery_tariff2', datetime.datetime(2022, 5, 3)))
+    # print(calculate_meter_total('delivery_tariff2', datetime.datetime(2022, 5, 3)))
 
 
 cli = cli.Cli()
@@ -80,7 +80,9 @@ else:
     delta = timedelta(days=1)
     while start_date <= end_date:
         print(start_date.strftime("%Y-%m-%d"))
+        start_day = datetime.combine(start_date, datetime.min.time())
+        end_day = datetime.combine(start_date, datetime.min.time())
 
-        day_statistics(datetime.combine(start_date, datetime.min.time()))
+        statistics(start_day, end_day)
 
         start_date += delta
